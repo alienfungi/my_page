554962f72ffb980cbef0e57d81c298ef435371cc
Zane <pzwoodfin@gmail.com>
2013-05-07 12:53:30 -0400

    Added temporary packman page.  Tweaked footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
 			"#{base_title} | #{page_title}"
 		end
 	end
+
+	# sets heading to :heading, :title, or "Zane's Site" in that priority
+	def heading(page_heading, page_title)
+		default_heading = "Zane's Site"
+		if page_heading.empty? && page_title.empty?
+			default_heading
+		elsif page_heading.empty?
+			page_title
+		else
+			page_heading
+		end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 MyPage::Application.routes.draw do
   root to: 'static_pages#home'
 
+  match '/packman', to: 'static_pages#packman'
+
+  # match '/about',   to: 'static_pages#about'
+  # match '/contact', to: 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
