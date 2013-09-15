2883baf2a7a426b3d242614eed0c5a53310082c2
Zane <pzwoodfin@gmail.com>
2013-09-15 13:36:54 -0400

    Fixes friending issues and adds buttons to friends index page.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,10 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    @friendship = if params.has_key? :friend_id
+      current_user.friendships.build(friend_id: params.delete(:friend_id))
+    else
+      current_user.friendships.build(friend_id: params.delete(:user_id))
+    end
     if @friendship.save
       flash[:success] = "Added friend."
       redirect_to users_path
@@ -29,7 +33,7 @@ class FriendshipsController < ApplicationController
   end
 
   def index
-    inbound_friends = current_user.inverse_friendships.map { |friendship| friendship.friend }
+    inbound_friends = current_user.inverse_friendships.map { |friendship| friendship.user }
     outbound_friends = current_user.friendships.map { |friendship| friendship.friend }
 
     @mutual = current_user.friendships.keep_if do |friendship|
@@ -37,7 +41,7 @@ class FriendshipsController < ApplicationController
     end
 
     @requests = current_user.inverse_friendships.reject do |friendship|
-      outbound_friends.include? friendship.friend
+      outbound_friends.include? friendship.user
     end
 
     @pending = current_user.friendships.reject do |friendship|
