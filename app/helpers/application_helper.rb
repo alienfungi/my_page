1da256a4f96ccdbd3f684e46e08dcf103a06e7c5
Zane <pzwoodfin@gmail.com>
2013-10-08 16:25:41 -0400

    Adds read/unread status to messages and notification badges to messages and friend requests.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,4 +38,33 @@ module ApplicationHelper
       "http://localhost:3000/"
     end
   end
+
+  def header_links
+    # check for new friend requests
+    inbound_friends = current_user.inverse_friendships.map { |friendship| friendship.user }
+    mutual_friends = current_user.friendships.where(friend_id: inbound_friends)
+    new_friend_count = inbound_friends.size - mutual_friends.size
+
+    # check for new messages
+    new_message_count = current_user.received_messages.where(read: false).count
+
+    # create links
+    {
+      "My Profile" => current_user,
+      "Activity" => activities_path,
+      "Users" => users_path,
+      "Friends#{create_badge(new_friend_count)}" => friendships_path,
+      "Messages#{create_badge(new_message_count)}" => messages_path
+    }
+  end
+
+private
+
+  def create_badge(num)
+    if num > 0
+      " <span class='badge'>#{num}</span>"
+    else
+      ""
+    end
+  end
 end
